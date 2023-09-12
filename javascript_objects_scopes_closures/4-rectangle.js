5822d935c47d7685923312fde36119764153e9ea
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-09-12 17:36:18 +0100

    Javascript objects, scopes, and closures

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,13 +1,14 @@
 class Rectangle{
     width;
     height;
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-         return {};  
-      }
+    constructor(w, h){
         this.width = w;
         this.height = h;
-      }
+    }
+    islessthanzero(){
+        if (this.width, this.height <= 0){
+            const obj = {}
+        }
     }
     print(){
         for (let i = 0; i < this.height; i++){
@@ -23,4 +24,3 @@ class Rectangle{
      this.height *= 2;   
     }
 }
-module.exports = Rectangle;
--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,8 @@
-const Rectangle = require('./4-rectangle');
-
-class Square extends Rectangle {
-  constructor(size) {
+import Rectangle from "./4-rectangle";
+class Square extends Rectangle{
+    size;
+    constructor(size){
     super(size, size);
-  }
+    }
 }
-
-module.exports = Square;
+module.exports = Rectangle;
