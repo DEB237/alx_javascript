aea48ae20367e3a292a9b28d02e8718feb126521
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-09-12 19:03:03 +0100

    Javascript objects, scopes, and closures

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
-      if (w <= 0 || h <= 0 ) {
-       return {};  
-    }
+      if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+        return {};
+      }
       this.width = w;
       this.height = h;
     }
