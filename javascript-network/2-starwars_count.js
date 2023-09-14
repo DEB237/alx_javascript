f5b8eb94e9812f511e33dfdc8979cb13d5be3eda
Dounla Enricka Bernie <dounlabernie@gmail.com>
2023-09-14 21:05:36 +0100

    JavaScript network

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`
+const apiUrl = process.argv[2]; // Get the API URL from command line arguments
+const characterId = 18; // Character ID for Wedge Antilles
 
-request.get(url, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-    } else {
-      const films = JSON.parse(body).results;
-      const moviesWithWedge = films.filter((film) => 
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    const films = JSON.parse(body).results;
+    const count = films.filter((film) =>
       film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-      );
-      console.log(moviesWithWedge.length);
-    }
-  });
+    ).length;
+    console.log(count);
+  }
+});
